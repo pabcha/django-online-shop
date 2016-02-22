468d70444fc5790e67e204db7e7bd626746b3e1d
PabloFarfan <pablochavez15@gmail.com>
2016-02-22 14:31:10 -0300

    Listar y ver productos

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True)
@@ -11,6 +12,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=[self.slug])
 
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products')
@@ -30,3 +34,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])
