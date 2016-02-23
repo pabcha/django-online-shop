272e7b9e05dc9f20aeee435aa64fa3064d4b9d6f
PabloFarfan <pablochavez15@gmail.com>
2016-02-23 13:30:36 -0300

    Se puede modificar la cantidad a llevar en el carrito

--- cart/views.py
+++ cart/views.py
@@ -23,4 +23,9 @@ def cart_remove(request, product_id):
 
 def cart_detail(request):
     cart = Cart(request)
+    for item in cart:
+        item['update_quantity_form'] = CartAddProductForm(initial={
+            'quantity': item['quantity'],
+            'update': True
+        })
     return render(request, 'cart/detail.html', {'cart': cart})
