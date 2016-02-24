899ffb07e8f3c77370c6a794e119817beac65e86
PabloFarfan <pablochavez15@gmail.com>
2016-02-24 20:47:39 -0300

    Aplicar descuento a las ordenes

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 from shop.models import Product
+from coupons.models import Coupon
 
 class Order(models.Model):
     first_name = models.CharField(max_length=50)
@@ -11,6 +13,12 @@ class Order(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
+    coupon = models.ForeignKey(Coupon,
+        related_name='orders',
+        null = True,
+        blank = True)
+    discount = models.IntegerField(default=0,
+        validators = [MinValueValidator(0), MaxValueValidator(100)])
 
     class Meta:
         ordering = ('-created',)
@@ -19,7 +27,8 @@ class Order(models.Model):
         return 'Order {}'.format(self.id)
 
     def get_total_cost(self):
-        return sum(item.get_cost() for item in self.items.all())
+        total_cost = sum(item.get_cost() for item in self.items.all())
+        return total_cost - total_cost * (self.discount / Decimal('100'))
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name='items')
